fc6504cbb3c0848ddca96113163f16aba058d287
Raul Barrera <rbarrera1992@gmail.com>
2020-10-22 11:39:11 -0500

    completed express boilerplate

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const app = require('./app');
-
-const PORT = process.env.PORT || 8000;
+const  { PORT } = require('./config');
 
 app.listen(PORT, () => {
   console.log(`Server listening in ${process.env.NODE_ENV} 
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { expect } = require('chai');
-const supertest = require('supertest');
 const app = require('../src/app');
 
 describe('App', () => {
