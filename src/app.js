b28eb8325876c2c8f48aa93d15882f3e209935df
Raul Barrera <rbarrera1992@gmail.com>
2020-10-23 11:12:33 -0500

    Added logger and API key

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-'use strict';
-
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const apiKey = require('./apiKey');
 
 const app = express();
 
@@ -16,6 +15,8 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(apiKey);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-'use strict';
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development'
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN: process.env.API_TOKEN
 };
