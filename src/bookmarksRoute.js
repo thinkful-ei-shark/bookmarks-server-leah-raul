98b7530e3301f8cd925491032dfed333b8c00ad3
Raul Barrera <rbarrera1992@gmail.com>
2020-10-23 14:24:31 -0500

    quote issues

--- src/bookmarksRoute.js
+++ src/bookmarksRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('./logger');
 const bookmarks = require('./bookmarksData.json');
 const { v4: uuid } = require('uuid');
-const {PORT} = require('./config');
+const { PORT } = require('./config');
 
 const bookmarksRouter = express.Router();
 const bodyParser = express.json();
@@ -13,92 +13,70 @@ bookmarksRouter
     res.status(200).json(bookmarks);
   })
   .post(bodyParser, (req, res) => {
-    console.log('inside of post')
-     const { title, rating, desc, url } = req.body;
-    console.log( title, rating, desc, url )
+    console.log('inside of post');
+    const { title, rating, desc, url } = req.body;
+    console.log(title, rating, desc, url);
 
-    if(!title){
-      logger.error('Title is required')
-      return res
-              .status(400)
-              .send('Title is required')
+    if (!title) {
+      logger.error('Title is required');
+      return res.status(400).send('Title is required');
     }
-    if(!rating){
-      logger.error('rating is required')
-      return res
-              .status(400)
-              .send('rating is required')
+    if (!rating) {
+      logger.error('rating is required');
+      return res.status(400).send('rating is required');
     }
-    if(!desc){
-      logger.error('desc is required')
-      return res
-              .status(400)
-              .send('desc is required')
+    if (!desc) {
+      logger.error('desc is required');
+      return res.status(400).send('desc is required');
     }
-    if(!url){
-      logger.error('url is required')
-      return res
-              .status(400)
-              .send('url is required')
+    if (!url) {
+      logger.error('url is required');
+      return res.status(400).send('url is required');
     }
 
-    const newBookmark = { id: uuid() ,title, rating, url, desc }
+    const newBookmark = { id: uuid(), title, rating, url, desc };
 
-
-
-    bookmarks.push(newBookmark)
+    bookmarks.push(newBookmark);
 
     res
       .status(201)
       .location(`http://localhost:${PORT}/bookmarks/${newBookmark.id}`)
-      .json(newBookmark)
-
-  })
-
+      .json(newBookmark);
+  });
 
 bookmarksRouter
   .route('/bookmarks/:id')
   .get((req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
 
-    console.log('inside get id')
-    const bookmark = bookmarks.find( bookmark => bookmark.id == id)
-    console.log(id)
-    console.log(bookmark)
+    console.log('inside get id');
+    const bookmark = bookmarks.find((bookmark) => bookmark.id == id);
+    console.log(id);
+    console.log(bookmark);
 
-    if(!bookmark) {
-      logger.error(`Bookmark with id ${id} not found`)
-      return res
-              .status(404)
-              .send('Bookmark not found')
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found`);
+      return res.status(404).send('Bookmark not found');
     }
 
-    res.json(bookmark)
+    res.json(bookmark);
   })
   .delete((req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
 
-    const bookmarkIndex = bookmarks.findIndex( bookmark => bookmark.id == id)
+    const bookmarkIndex = bookmarks.findIndex((bookmark) => bookmark.id == id);
 
-    console.log(bookmarkIndex, id, bookmarks[3])
-    
+    console.log(bookmarkIndex, id, bookmarks[3]);
 
-    if(bookmarkIndex === -1) {
-      logger.error(`Bookmark with id ${id} not found`)
-      return res
-              .status(404)
-              .send('Bookmark not found')
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found`);
+      return res.status(404).send('Bookmark not found');
     }
 
+    bookmarks.splice(bookmarkIndex, 1);
 
-    bookmarks.splice(bookmarkIndex , 1)
-
-    logger.info(`Bookmark with id: ${id} has been deleted`)
-    res
-      .status(204)
-      .end()
-    
-  })
-  
+    logger.info(`Bookmark with id: ${id} has been deleted`);
+    res.status(204).end();
+  });
 
 module.exports = bookmarksRouter;
